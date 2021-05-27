6e41e4978010240e6c60b289ce5e20fc5b487837
Joren <63969493+joren961@users.noreply.github.com>
2021-05-27 17:47:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,14 @@ FLASK_APP = __name__
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'This is where the preferences are set'
+
+
+@app.route('/game/<nickname>')
+def game(nickname):
+    return 'This is where the game is played'
+
+
+@app.route('/')
+def statistics():
+    return 'This is where the statistics are displayed'
