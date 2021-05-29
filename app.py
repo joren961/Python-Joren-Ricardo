8ec4a6e74109e6d7bc290c9f9972ed2fc8b94d29
Joren <63969493+joren961@users.noreply.github.com>
2021-05-29 15:39:23 +0200

    formchanges

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import development as development
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 FLASK_APP = __name__
 FLASK_ENV = development
 
@@ -12,11 +12,23 @@ def home():
     return render_template('homepage.html')
 
 
-@app.route('/game/<nickname>')
-def game(nickname):
-    return 'This is where the game is played'
+@app.route('/game', methods=['GET'])
+def game():
+    nickname = request.form.get("nickname")
+    doubleColors = request.form.get("double-colors")
+    colorAmount = request.form.get("color-amount")
+    positionAmount = request.form.get("position-amount")
+    return render_template('game.html',
+                           nickname=nickname,
+                           doubleColors=doubleColors,
+                           colorAmount=colorAmount,
+                           positionAmount=positionAmount)
 
 
 @app.route('/leaderboard')
 def statistics():
     return render_template('leaderboard.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
